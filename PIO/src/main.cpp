685296168c12e24d46d4889e25afe451636117d7
Comrade Pingu <alexandre@zipion-live.be>
2026-03-20 22:31:05 +0100

    Uncomment soil moisture sensor and temperature sensor

--- PIO/src/main.cpp
+++ PIO/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
-// #include "SoilMoistureSensor.h"
-// #include "TemperatureSensor.h"
+#include "SoilMoistureSensor.h"
+#include "TemperatureSensor.h"
 #include "WaterLevelSensor.h"
 
 #define SMS_PIN A0
@@ -8,25 +8,25 @@
 #define WLS_HIGH_ADDR 0x78
 #define WLS_LOW_ADDR 0x77
 
-// SoilMoistureSensor sms(SMS_PIN);
+SoilMoistureSensor sms(SMS_PIN);
 WaterLevelSensor wls(WLS_LOW_ADDR, WLS_HIGH_ADDR);
-// TemperatureSensor ts(TS_PIN);
+TemperatureSensor ts(TS_PIN);
 
 unsigned long previousMillis = 0;
 const unsigned long interval = 500;
 
 void readValues() {
-    // const int humidity = sms.readPercentage();
-    // const float temperature = ts.readTemperature();
+    const int humidity = sms.readPercentage();
+    const float temperature = ts.readTemperature();
     const float waterLevel = wls.readWaterLevel();
-    // Serial.write("Humidity : ");
-    // Serial.write(humidity);
-    // Serial.write("%");
-    // Serial.println();
-    // Serial.write("Temperature : ");
-    // Serial.print(temperature, 2);
-    // Serial.write("°C");
-    // Serial.println();
+    Serial.write("Humidity : ");
+    Serial.write(humidity);
+    Serial.write("%");
+    Serial.println();
+    Serial.write("Temperature : ");
+    Serial.print(temperature, 2);
+    Serial.write("°C");
+    Serial.println();
     if (waterLevel < 0) {
         Serial.println("Water level sensor not found!");
     } else {
