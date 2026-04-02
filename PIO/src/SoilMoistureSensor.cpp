a0c392404d136076765471a1112b694f72a2ccb7
Comrade Pingu <alexandre@zipion-live.be>
2026-04-02 23:19:27 +0200

    Using full scale for the humidity sensor

--- PIO/src/SoilMoistureSensor.cpp
+++ PIO/src/SoilMoistureSensor.cpp
@@ -9,7 +9,7 @@ SoilMoistureSensor::SoilMoistureSensor(uint8_t analogPin) : _analogPin(analogPin
 int SoilMoistureSensor::readPercentage() {
     int raw = analogRead(_analogPin);
 
-    if (raw <= 320) { raw = 320; }
+    //if (raw <= 320) { raw = 320; }
 
-    return map(raw, 1023, 320, 0, 100);
+    return map(raw, 1023, 0, 0, 100);
 }
